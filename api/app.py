4377f6906c06d741f3db420a97f29dc6c90d107d
Stephen Leyva <sleyva@staq.com>
2019-03-06 20:30:48 -0500

    Remove server name conf
    * Will ignore other network interfaces

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
-# import logging.config
-import logging
+import logging.config
+
 from flask import Flask
 
 from api import settings
@@ -15,7 +15,7 @@ log = logging.getLogger(__name__)
 
 
 def configure_app(flask_app):
-    flask_app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
+    # flask_app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI # NOQA
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS # NOQA
     flask_app.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTPLUS_SWAGGER_UI_DOC_EXPANSION # NOQA
@@ -32,7 +32,7 @@ def initialize_app(flask_app):
 
 def main():
     initialize_app(app)
-    app.run(debug=settings.FLASK_DEBUG)
+    app.run(debug=settings.FLASK_DEBUG, host='0.0.0.0')
 
 
 if __name__ == "__main__":
