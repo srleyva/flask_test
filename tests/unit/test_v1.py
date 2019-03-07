1914195f7db41d5b238dc0dbe9e8c935be7dd326
Stephen Leyva <sleyva@staq.com>
2019-03-07 12:05:08 -0500

    Add return value for job stuff

--- tests/unit/test_v1.py
+++ tests/unit/test_v1.py
@@ -1,4 +1,6 @@
 """Unit tests for the Jobs API v1 Service endpoint"""
+import datetime
+import json
 import unittest
 from unittest.mock import Mock
 
@@ -8,6 +10,14 @@ from api.services.job_queue import JobQueue
 # Initialize Routes
 mock_job_queue = Mock(JobQueue)
 api.initialize_app(api.app, mock_job_queue)
+
+create = datetime.datetime.now()
+test_job = {
+    "id": 1,
+    "state": "running",
+    "priority": 1,
+    # "created_at": create
+}
 
 
 class V1ServiceTest(unittest.TestCase):
@@ -30,15 +40,25 @@ class V1ServiceTest(unittest.TestCase):
 
     def test_jobs_peek(self):
         '''Test GET method on /v1/jobs/'''
-        self.mock_job_queue.peek.return_value = []
+        self.mock_job_queue.peek.return_value = test_job
         response = self.app.get('/v1/jobs/')
 
-        self.mock_job_queue.peek.assert_called()
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.data), test_job)
+        self.mock_job_queue.peek.assert_called()
 
     def test_job_refresh(self):
         '''Test PUT method on /v1/jobs/'''
         response = self.app.put('/v1/jobs/')
 
-        self.mock_job_queue.refresh.assert_called()
         self.assertEqual(response.status_code, 200)
+        self.mock_job_queue.refresh.assert_called()
+
+    def test_job_pop(self):
+        '''Test DELETE method on /v1/jobs/'''
+        self.mock_job_queue.pop.return_value = test_job
+        response = self.app.delete('/v1/jobs/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.data), test_job)
+        self.mock_job_queue.pop.assert_called()
