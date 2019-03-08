9789a1dec98a4125bef829f511212c3e9db7c33f
Stephen Leyva <sleyva@staq.com>
2019-03-08 16:07:23 -0500

    Allow setting to be set in ENV

--- api/settings.py
+++ api/settings.py
@@ -1,6 +1,8 @@
+import os
+
 # Flask settings
-FLASK_SERVER_NAME = 'localhost:8888'
-FLASK_DEBUG = False  # Do not use debug mode in production
+FLASK_SERVER_NAME = os.environ.get('API_SERVER_NAME', 'localhost:8888')
+FLASK_DEBUG = os.environ.get('API_DEBUG', False)
 
 # Flask-Restplus settings
 RESTPLUS_SWAGGER_UI_DOC_EXPANSION = 'list'
@@ -9,8 +11,10 @@ RESTPLUS_MASK_SWAGGER = False
 RESTPLUS_ERROR_404_HELP = False
 
 # SQLAlchemy settings
-SQLALCHEMY_DATABASE_URI = 'postgres://postgres:@postgres:5432/circle_test'
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    'API_DATABASE_URI',
+    'postgres://postgres:@postgres:5432/circle_test')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # JWT
-JWT_SECRET_KEY = 'secret-key'
+JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'secret-key')
